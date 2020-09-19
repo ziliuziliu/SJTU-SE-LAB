f1217aff42c6b354ea54f7aff91a5230db14536b
ziliuziliu <851123942@qq.com>
2020-09-19 16:23:11 +0800

    cse-lab1 commit5

--- lab-cse/lab1/inode_manager.cc
+++ lab-cse/lab1/inode_manager.cc
@@ -95,11 +95,12 @@ inode_manager::alloc_inode(uint32_t type)
   for (int i=1;i<INODE_NUM;i++) {
     inode* ino = get_inode(i);
     if (ino == NULL) {
+      ino = new inode();
       ino->type = type;
       ino->size = 0;
       ino->atime = ino->ctime = ino->mtime = 0;
       put_inode(i, ino);
-      break;
+      return i;
     }
   }
   return 0;
